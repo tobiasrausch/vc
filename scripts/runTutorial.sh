7385bbd877a3bbc496ae2c3c4d6a12badf8cd6d8
tobiasrausch <rauschtobi@gmail.com>
2018-05-24 14:43:12 +0200

    check that docker is accessible

--- scripts/runTutorial.sh
+++ scripts/runTutorial.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-docker pull trausch/variant-calling
-docker run -d -it -e DISPLAY=${DISPLAY} -u ubuntu -v /tmp/.X11-unix/:/tmp/.X11-unix:ro trausch/variant-calling /bin/bash
-export containerId=`docker ps -l -q`
-xhost +local:`docker inspect --format='{{ .Config.Hostname }}' $containerId`
-echo "Press Enter to start container..."
-docker start -i $containerId
+docker -v
+if [ $? -eq 0 ]
+then
+    docker pull trausch/variant-calling
+    if [ $? -eq 0 ]
+    then
+	docker run -d -it -e DISPLAY=${DISPLAY} -u ubuntu -v /tmp/.X11-unix/:/tmp/.X11-unix:ro trausch/variant-calling /bin/bash
+	export containerId=`docker ps -l -q`
+	xhost +local:`docker inspect --format='{{ .Config.Hostname }}' $containerId`
+	echo "Press Enter to start container..."
+	docker start -i $containerId
+    fi
+fi
