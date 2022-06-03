164dad7ce437aa5756d5e691aa064d1dacd4e725
tobiasrausch <rauschtobi@gmail.com>
2022-06-03 13:07:51 +0200

    test

--- test.sh
+++ test.sh
@@ -5,4 +5,9 @@ then
     cat README.md | awk '/^```bash$/,/^```$/  {print} {next}' | grep -v '^`' | grep -v "^igv" > run.sh
     chmod a+x run.sh
     ./run.sh
+    if [ $? -eq 0 ]
+    then
+	echo "Test completed successfully!"
+    fi
+    rm run.sh
 fi
